2cb0cfba47aa09086f0e4e25885cce1b3a731a0b
Sarthak Srivastav <sarthak.sri0203@gmail.com>
2026-04-29 16:03:32 +0530

    basic file manager completed

--- file/file_manager.go
+++ file/file_manager.go
@@ -86,6 +86,119 @@ func (m *Manager) Read(block *BlockID, page *Page) error {
 	return nil
 }
 
+// Write to the disk from the page (memory) to the file
+func (m *Manager) Write(block *BlockID, page *Page) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	file, err := m.getFile(block.Filename())
+	if err != nil {
+		return fmt.Errorf("cannot write block %s: %v", block.String(), err)
+	}
+
+	offset := int64(block.Number())*int64(m.blocksize)
+	if _,err := file.Seek(offset,io.SeekStart); err != nil {
+		return fmt.Errorf("cannot seek to offset %d: %v", offset, err)
+	}
+
+	//write to the disk
+	buffer := page.Contents()
+	n,err := file.Write(buffer)
+	if err != nil {
+		if n!=len(buffer){
+			return fmt.Errorf("short write: expected %d bytes, wrote %d, %v", len(buffer), n, err)
+		}
+		return fmt.Errorf("cannot write data: %v",err)
+	}
+
+	// flush data to the disk and cache
+	if err := file.Sync(); err != nil {
+		return fmt.Errorf("cannot flush file %s to disk: %v", block.Filename(), err)
+	}
+
+	return nil
+}
+
+// appends a new block to the file and return blockID
+// append creates block -> returns blockID -> write into that block
+/*
+	File has N blocks
+        ↓
+	Append called
+        ↓
+	New block = N
+        ↓
+	Write empty block at offset N × blockSize
+        ↓
+	File now has N+1 blocks
+*/
+func(m *Manager) Append(filename string) (BlockID, error){
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	newBlockNumber,err := m.length(filename)
+	if err != nil {
+		return BlockID{},fmt.Errorf("cannot get length of %s: %v", filename, err)
+	}
+
+	block := BlockID{filename: filename, blockNum: newBlockNumber}
+
+	f, err := m.getFile(filename)
+	if err != nil {
+		return BlockID{},fmt.Errorf("cannot append block %s: %v", block.String(), err)
+	}
+
+	offset := int64(block.Number()) * int64(m.blocksize)
+	if _,err := f.Seek(offset,io.SeekStart); err != nil {
+		return BlockID{},fmt.Errorf("cannot seek to offset %d: %v", offset, err)
+	}
+
+	b := make([]byte,m.blocksize)
+	n,err := f.Write(b)
+
+	if err != nil {
+		return BlockID{},fmt.Errorf("cannot write data: %v", err)
+	}
+
+	if n != len(b) {
+		return BlockID{}, fmt.Errorf("short write: expected %d bytes, wrote %d", len(b), n)
+	}
+
+	// Ensure the data is flushed to disk.
+	if err := f.Sync(); err != nil {
+		return BlockID{}, fmt.Errorf("cannot sync file %s: %v", filename, err)
+	}
+
+	return block, nil
+}
+
+// length returns the number of blocks in the file.
+func (m *Manager) length(filename string) (int, error){
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	file, err := m.getFile(filename)
+	if err != nil {
+		return 0, fmt.Errorf("cannot get file %s: %v", filename, err)
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		return 0, fmt.Errorf("cannot stat file %s: %v", filename, err)
+	}
+
+	fileSizeInBytes := info.Size()
+	return int(fileSizeInBytes / int64(m.blocksize)), nil
+}
+
+func (m *Manager) IsNew() bool {
+	return m.isNew
+}
+
+// BlockSize returns the block size used by the FileMgr.
+func (m *Manager) BlockSize() int {
+	return m.blocksize
+}
 
 // getFile retrieves or opens a file and stores it in the openFiles map.
 func (m *Manager) getFile(filename string) (*os.File, error) {
